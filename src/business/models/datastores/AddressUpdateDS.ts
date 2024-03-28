4a26da39544858d68e0f8b8a2221149a4094a59e
Dimitri Steinbusch <dimitri.steinbusch@creatiview.be>
2024-03-29 00:13:47 +0100

    [CS-16] fix build error

--- src/business/models/datastores/AddressUpdateDS.ts
+++ src/business/models/datastores/AddressUpdateDS.ts
@@ -4,8 +4,8 @@ export default class AddressUpdateDS extends AddressCreationDS {
     private readonly addressId: string;
 
 
-    constructor(countryId: number, userId: string, basketId: string, city: string, street: string, streetNumber: string, box: string, addressId: string) {
-        super(countryId, userId, basketId, city, street, streetNumber, box);
+    constructor(countryId: number, userId: string, city: string, street: string, streetNumber: string, box: string, addressId: string) {
+        super(countryId, userId, city, street, streetNumber, box);
         this.addressId = addressId;
     }
 
