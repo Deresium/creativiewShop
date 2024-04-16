446732fa19174d8b3306c3110be6293e7df7d7ce
Dimitri Steinbusch <dimitri.steinbusch@uliege.be>
2024-04-16 10:48:25 +0200

    Adapting tests due to change to decimal

--- src/test/percentCalculator.test.ts
+++ src/test/percentCalculator.test.ts
@@ -4,14 +4,14 @@ import Decimal from "decimal.js";
 describe('Test Percent Calculator', () => {
     test('original price 1000 discount 800', () => {
         const percent = PercentCalculator.calculatePercentBasedOnPrices(new Decimal(1000), new Decimal(800));
-        expect(percent).toBe(20);
+        expect(percent.equals(20)).toBe(true);
     });
     test('original price 500 discount 0', () => {
         const percent = PercentCalculator.calculatePercentBasedOnPrices(new Decimal(500), new Decimal(0));
-        expect(percent).toBe(100);
+        expect(percent.equals(100)).toBe(true);
     });
     test('original price 500 discount 800', () => {
         const percent = PercentCalculator.calculatePercentBasedOnPrices(new Decimal(500), new Decimal(800));
-        expect(percent).toBe(-60);
+        expect(percent.equals(-60)).toBe(true);
     });
 });
