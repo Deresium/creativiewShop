c4f2359ad69b2c4fdd84eb71f0c6ec64765132ab
Dimitri Steinbusch <dimitri.steinbusch@creatiview.be>
2024-11-28 18:25:10 +0100

    Fix bug store search when there is only one element

--- src/routers/routes/StoreRouter.ts
+++ src/routers/routes/StoreRouter.ts
@@ -86,8 +86,12 @@ export default class StoreRouter extends ApplicationRouter {
                 const customer = req.customer;
                 const searchTerm = req.query.searchTerm;
                 const categoryIds = req.query["categoryIds[]"];
+                const categoryArrayIds = Array.isArray(categoryIds) ? categoryIds : [categoryIds];
+
                 const orderBy = req.query.orderBy;
                 const manufacturerIds = req.query["manufacturerIds[]"];
+                const manufacturerArrayIds = Array.isArray(manufacturerIds) ? manufacturerIds : [manufacturerIds];
+
                 const language = req.query.language;
                 const groups = req.userGroups;
                 const currency = req.query.currency;
